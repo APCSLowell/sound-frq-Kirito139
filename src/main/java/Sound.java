8c48291ecc5e40605e9bead1e3f4498a3ec49877
Lennier Mulder <lenniermulder@icloud.com>
2025-05-06 23:03:22 -0700

    First attempt

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -1,33 +1,53 @@
 import java.util.*;
 public class Sound
 {
-  /** the array of values in this sound; guaranteed not to be null */
-  int[] samples;
+    /** the array of values in this sound; guaranteed not to be null */
+    int[] samples;
 
 
 
-  /** Changes those values in this sound that have an amplitude greater than limit.
-   *  Values greater than limit are changed to limit.
-   *  Values less than -limit are changed to -limit.
-   *  @param limit the amplitude limit
-   *         Precondition: limit >= 0
-   *  @return the number of values in this sound that this method changed
-   */
-  public int limitAmplitude(int limit)
-  {  
-    /* to be implemented in part (a) */
-    return 0;
-  }
+    /** Changes those values in this sound that have an amplitude greater than limit.
+     *  Values greater than limit are changed to limit.
+     *  Values less than -limit are changed to -limit.
+     *  @param limit the amplitude limit
+     *         Precondition: limit >= 0
+     *  @return the number of values in this sound that this method changed
+     */
+    public int limitAmplitude(int limit) {  
+        /* to be implemented in part (a) */
+        int changecount = 0;
+        for (int i = 0; i < samples.length; i++) {
+
+            if (samples[i] < -limit) {
+                samples[i] = -limit;
+                changecount++;
+            } else if (samples[i] > limit) {
+                samples[i] = limit;
+                changecount++;
+            }
+        }
+        return changecount;
+    }
 
 
 
-  /** Removes all silence from the beginning of this sound.
-   *  Silence is represented by a value of 0.
-   *  Precondition: samples contains at least one nonzero value
-   *  Postcondition: the length of samples reflects the removal of starting silence
-   */
-  public void trimSilenceFromBeginning()
-  {
-    /* to be implemented in part (b) */
-  }
+    /** Removes all silence from the beginning of this sound.
+     *  Silence is represented by a value of 0.
+     *  Precondition: samples contains at least one nonzero value
+     *  Postcondition: the length of samples reflects the removal of starting silence
+     */
+    public void trimSilenceFromBeginning() {
+        /* to be implemented in part (b) */
+        int i = 0;
+
+        while (samples[i] == 0) {
+            i++;
+        }
+
+        samples = new int[samples.length - i];
+
+        for (int j = 0; j < samples.length; j++) {
+            samples[j] = samples[j + i];
+        }
+    }
 }
